b4187fc1ada16ebed44ac4aaf5d55168e58a7aba
Om singh <om.ajit.singh81@gmail.com>
2025-09-05 23:27:06 +0530

    Add files via upload

---
+++ ARRAY Problems With Leetcode/repeatingDigit.java
@@ -0,0 +1,22 @@
+// import java.util.*; 
+// public class repeatingDigit {
+//     public static void main(String[] args) {
+//         Scanner sc= new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int t = sc.nextInt();
+//         int result=frequency(n, t);
+//         System.out.println(result);
+//     }
+//     public static int frequency(int n,int t){
+//         int count=0;
+//         if (n==0 && t==0){
+//             return 1;
+//         }
+//         while (n>0) {
+//             int rem=n%10;
+//             if (rem==t) {
+//                 count++;
+//             }n/=10;
+//         }return count;
+//     }
+// }
