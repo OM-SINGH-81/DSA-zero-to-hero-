10cf9af4787f713198b263c4cf94905b362de0ec
Om singh <om.ajit.singh81@gmail.com>
2025-09-11 23:34:14 +0530

    Add files via upload

---
+++ ARRAY Problems With Leetcode/NextPermutation.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class NextPermutation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        
+        int left = i + 1, right = n - 1;
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+
+        // Print result
+        for (int k = 0; k < n; k++) {
+            System.out.print(nums[k] + " ");
+        }
+    }
+}
