927c07ed4e0576663eb775e158e2357cc4e41524
Om singh <om.ajit.singh81@gmail.com>
2025-09-20 22:44:22 +0530

    Update Maximum_circular_subarray.java

--- ARRAY Problems With Leetcode/Maximum_circular_subarray.java
+++ ARRAY Problems With Leetcode/Maximum_circular_subarray.java
@@ -1,3 +1,42 @@
-public class Maximum_circular_subarray {
-    
+import java.util.Scanner;
+ public class Maximum_circular_sum {
+    public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+        int t = name.nextInt();
+        while (t-- > 0) {
+            int n = name.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = name.nextInt();
+
+            System.out.println(subarray(arr));
+        }
+    }
+
+    public static int subarray(int[] arr) {
+		int totalsum=0;
+		int ansmax=Integer.MIN_VALUE;
+		int ansmin=Integer.MAX_VALUE;
+		int max= 0;
+		int min=0;
+		for (int i = 0; i < arr.length; i++) {
+			max+=arr[i];
+			min+=arr[i];
+			ansmax=Math.max(max, ansmax);
+			ansmin=Math.min(min, ansmin);
+			
+			totalsum+=arr[i];
+			if(max<0) {
+				max=0;
+			}
+			if(min>0) {
+				min=0;
+			}
+			
+		}
+		if(ansmax<0) return ansmax;
+		return Math.max(totalsum-ansmin,ansmax);
+		
+	}
+
 }
